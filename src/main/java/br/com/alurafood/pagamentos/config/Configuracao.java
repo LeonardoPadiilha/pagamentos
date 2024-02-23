ad100d610813d21b90f734f4f6c17104e9efe7ef
Leonardo Padilha <leoprsouza@hotmail.com>
2024-02-23 18:13:05 -0300

    corrigindo feign usando load balancer

--- src/main/java/br/com/alurafood/pagamentos/config/Configuracao.java
+++ src/main/java/br/com/alurafood/pagamentos/config/Configuracao.java
@@ -1,8 +1,6 @@
 package br.com.alurafood.pagamentos.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.FeignContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
